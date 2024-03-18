27e4cb3b84f41750b68980fcc6c24a47d4b96b71
namhaitech <namhai.techsoft@gmail.com>
2024-03-18 16:55:47 +0700

    Add project files.

---
+++ NHG-GAME-CLIENT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NHG_GAME_CLIENT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
