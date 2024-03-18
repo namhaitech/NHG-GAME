50e1f06ed136b5a76097627375ea8da8190452ee
namhaitech <namhai.techsoft@gmail.com>
2024-03-18 18:17:57 +0700

    Check true word

---
+++ DataAccess/DataWord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class DataWord
+    {
+        public string Text { get; set; }
+        public List<string> Source { get; set; }
+    }
+}
---
+++ NHG-GAME-API/Controllers/API_ConnectWord.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using DataAccess;
+using System.Linq;
+
+namespace NHG_GAME_API.Controllers
+{
+    [Route("api/connectword")]
+    [ApiController]
+    public class API_ConnectWord : Controller
+    {
+        [HttpPost]
+        public IActionResult CheckWordIsTrue(string word)
+        {
+            string filePath = @"Data\data_words.txt";
+            var lines = System.IO.File.ReadAllLines(filePath);
+            var isValidWord = false;
+
+            foreach (var line in lines)
+            {
+                var wordItem = JsonConvert.DeserializeObject<DataWord>(line);
+                if (wordItem != null)
+                {
+                    int wordLength = wordItem.Text.Split(' ').Count();
+                    string words = wordItem.Text.ToLower();
+                    if (wordLength == 2 && words.Contains(word.ToLower()))
+                    {
+                        isValidWord = true;
+                        break;
+                    }
+                }
+            }
+
+            if (isValidWord)
+            {
+                return Ok(new { message = "Từ '"+word+"' hợp lệ", word = word, status = true });
+            }
+            else
+            {
+                return Ok(new { message = "Từ '"+word+"' không hợp lệ", word = word, status = false });
+            }
+        }
+    }
+}
